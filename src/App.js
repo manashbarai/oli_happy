72453d525bc6abe415b6fc69f5b1011a71d93273
manashbarai <manashjbarai@gmail.com>
2025-01-01 14:56:34 +0530

    remove build error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import Quiz from './components/Quiz';
+import HappyNewYear from './components/HappyNewYear';
 import './App.css';
 
 function App() {
+  const [quizCompleted, setQuizCompleted] = useState(false);
+
+  const handleQuizComplete = () => {
+    setQuizCompleted(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HappyNewYear />
+     
     </div>
   );
 }
